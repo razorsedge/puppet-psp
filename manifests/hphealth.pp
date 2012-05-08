2ded9f17c4673c964dfb7ad88ca11a5401e57e4f
Michael Arnold <github@razorsedge.org>
2012-05-07 23:41:12 -0700

    Paramerterize Class['psp::hphealth'].

--- manifests/hphealth.pp
+++ manifests/hphealth.pp
@@ -1,93 +1,115 @@
-# Class: psp::hphealth
+# == Class: psp::hphealth
 #
-# This class manages hphealth.
+# This class handles installation of the HP Proliant Support Pack Health Agent.
 #
-# Parameters:
+# === Parameters:
 #
-# Actions:
+# [*ensure*]
+#   Ensure if present or absent.
+#   Default: present
 #
-# Requires:
+# [*autoupgrade*]
+#   Upgrade package automatically, if there is a newer version.
+#   Default: false
 #
-# Sample Usage:
+# [*service_ensure*]
+#   Ensure if service is running or stopped.
+#   Default: running
 #
-class psp::hphealth {
+# [*service_enable*]
+#   Start service at boot.
+#   Default: true
+#
+# === Actions:
+#
+# Installs the HP System Health Application and Command Line Utilities.
+# Installs the RILOE II/iLO online configuration utility.
+# Installs the HP Command Line Array Configuration Utility.
+# Installs OpenIPMI drivers and/or HP iLO Channel Interface Driver dependent
+# upon OS version.
+#
+# === Requires:
+#
+# Class['psp']
+#
+# === Sample Usage:
+#
+#   class { 'psp::hphealth': }
+#
+# === Authors:
+#
+# Mike Arnold <mike@razorsedge.org>
+#
+# === Copyright:
+#
+# Copyright (C) 2012 Mike Arnold, unless otherwise noted.
+#
+class psp::hphealth (
+  $ensure         = 'present',
+  $autoupgrade    = false,
+  $service_ensure = 'running',
+  $service_enable = true,
+) inherits psp::params {
+
+  case $ensure {
+    /(present)/: {
+      if $autoupgrade == true {
+        $package_ensure = 'latest'
+      } else {
+        $package_ensure = 'present'
+      }
+
+      if $service_ensure in [ running, stopped ] {
+        $service_ensure_real = $service_ensure
+        $service_enable_real = $service_enable
+      } else {
+        fail('service_ensure parameter must be running or stopped')
+      }
+    }
+    /(absent)/: {
+      $package_ensure = 'absent'
+      $service_ensure_real = 'stopped'
+      $service_enable_real = false
+    }
+    default: {
+      fail('ensure parameter must be present or absent')
+    }
+  }
+
   package { 'hp-OpenIPMI':
-    ensure => 'present',
-    name   => $::operatingsystem ? {
-      /CentOS|RedHat|OEL|OracleLinux/ => $::operatingsystemrelease ? {
-        /6.*/      => 'OpenIPMI',
-        /5.[5-9]*/ => 'OpenIPMI',
-        default    => 'hp-OpenIPMI',
-      },
-      default                         => 'hp-OpenIPMI',
-    },
+    ensure => $package_ensure,
+    name   => $psp::params::ipmi_name,
   }
 
   package { 'hponcfg':
-    ensure => 'present',
-    name   => 'hponcfg',
+    ensure => $package_ensure,
   }
 
   package { 'hp-health':
-    ensure => 'present',
-    name   => 'hp-health',
-  }
-
-  package { 'hp-ilo':
-    ensure => $::operatingsystem ? {
-      /CentOS|RedHat|OEL|OracleLinux/ => $::operatingsystemrelease ? {
-        /6.*/      => 'absent',
-        /5.[3-9]*/ => 'absent',
-        default    => 'present',
-      },
-      default                         => 'present',
-    },
-    name   => 'hp-ilo',
+    ensure => $package_ensure,
   }
 
   package { 'hpacucli':
-    ensure => 'present',
-    name   => 'hpacucli',
+    ensure => $package_ensure,
+  }
+
+  package { 'hp-ilo':
+    ensure => $psp::params::ilo_package_ensure,
   }
 
   service { 'hp-ilo':
-    ensure     => $::operatingsystem ? {
-      /CentOS|RedHat|OEL|OracleLinux/ => $::operatingsystemrelease ? {
-        /6.*/      => undef,
-        /5.[3-9]*/ => undef,
-        default    => 'running',
-      },
-      default                         => 'running',
-    },
-    enable     => $::operatingsystem ? {
-      /CentOS|RedHat|OEL|OracleLinux/ => $::operatingsystemrelease ? {
-        /6.*/      => undef,
-        /5.[3-9]*/ => undef,
-        default    => true,
-      },
-      default                         => true,
-    },
+    ensure     => $psp::params::ilo_service_ensure,
+    enable     => $psp::params::ilo_service_enable,
     hasrestart => true,
     hasstatus  => true,
     require    => Package['hp-ilo'],
-    name       => 'hp-ilo',
   }
 
   service { 'hp-health':
-    ensure     => 'running',
-    enable     => true,
+    ensure     => $service_ensure_real,
+    enable     => $service_enable_real,
     hasrestart => true,
     hasstatus  => true,
     require    => [ Package['hp-health'], Package['hp-OpenIPMI'], ],
-    name       => 'hp-health',
   }
-
-#  service { 'hp-asrd':
-#    name       => 'hp-asrd',
-#    ensure     => 'running',
-#    enable     => true,
-#    hasrestart => true,
-#    hasstatus  => true,
-#    require    => Package['hp-health'],
-#  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -22,6 +22,32 @@ class psp::params {
     'CentOS': {
       $yum_os = 'CentOS'
       $vca_ensure = 'absent'
+      case $::operatingsystemrelease {
+        /4.*/: {
+          $ipmi_name = 'hp-OpenIPMI'
+          $ilo_package_ensure = 'present'
+          $ilo_service_ensure = 'running'
+          $ilo_service_enable = true
+        }
+        /5.[0-2]*/: {
+          $ipmi_name = 'hp-OpenIPMI'
+          $ilo_package_ensure = 'present'
+          $ilo_service_ensure = 'running'
+          $ilo_service_enable = true
+        }
+        /5.[3-4]*/: {
+          $ipmi_name = 'hp-OpenIPMI'
+          $ilo_package_ensure = 'absent'
+          $ilo_service_ensure = undef
+          $ilo_service_enable = undef
+        }
+        default: {
+          $ipmi_name = 'OpenIPMI'
+          $ilo_package_ensure = 'absent'
+          $ilo_service_ensure = undef
+          $ilo_service_enable = undef
+        }
+      }
     }
     'OracleLinux', 'OEL': {
       $yum_os = 'Oracle'
