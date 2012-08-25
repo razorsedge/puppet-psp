ab9645e5fd575f09f73bb0545a3e367beaefec0f
Michael Arnold <github@razorsedge.org>
2012-08-25 08:39:54 -0700

    Paramerterize Class['psp::hpsnmp'].
    Changes references to Class['snmpd'] to Class['snmp'].

--- manifests/hpsnmp.pp
+++ manifests/hpsnmp.pp
@@ -10,80 +10,117 @@
 #
 # Sample Usage:
 #
-class psp::hpsnmp {
-  include psp::snmp
-  #Class['psp::snmp'] -> Class['psp::hpsnmp']
+class psp::hpsnmp (
+  $ensure         = 'present',
+  $autoupgrade    = false,
+  $service_ensure = 'running',
+  $service_enable = true
+) inherits psp::params {
 
-  package { 'hp-snmp-agents':
-    ensure => 'present',
-    name   => 'hp-snmp-agents',
-#    require => Class['snmp'],
-    require => [ User['hpsmh'], Yumrepo['HP-psp'], ],
+  case $ensure {
+    /(present)/: {
+      if $autoupgrade == true {
+        $package_ensure = 'latest'
+      } else {
+        $package_ensure = 'present'
+      }
+      $file_ensure = 'present'
+      if $service_ensure in [ running, stopped ] {
+        $service_ensure_real = $service_ensure
+        $service_enable_real = $service_enable
+      } else {
+        fail('service_ensure parameter must be running or stopped')
+      }
+    }
+    /(absent)/: {
+      $package_ensure = 'absent'
+      $file_ensure = 'absent'
+      $service_ensure_real = 'stopped'
+      $service_enable_real = false
+    }
+    default: {
+      fail('ensure parameter must be present or absent')
+    }
   }
 
-  file { 'snmpd.conf-HP':
-    ensure  => 'present',
-    mode    => '0660',
-    owner   => 'root',
-    group   => 'hpsmh',
-    require => Package['snmpd'],
-    path    => '/etc/snmp/snmpd.conf-HP',
-    source  => [
-      "puppet:///modules/snmpd/snmpd.conf-${::fqdn}",
-      'puppet:///modules/snmpd/snmpd.conf',
-    ],
-   #notify  => Service['snmpd'],
-    notify  => Exec['copy_snmpd.conf-HP'],
-  }
+  case $::manufacturer {
+    'HP': {
+      #Package { require => Class['psp'], }
+      include psp
+      include psp::snmp
 
-  exec { 'copy_snmpd.conf-HP':
-    command     => '/bin/cp -p /etc/snmp/snmpd.conf-HP /etc/snmp/snmpd.conf',
-   #onlyif      => 'test -f /etc/snmp/snmpd.conf-HP',
-    require     => File['snmpd.conf-HP'],
-    notify      => Exec['hpsnmpconfig'],
-    refreshonly => true,
-  }
+      package { 'hp-snmp-agents':
+        ensure   => $package_ensure,
+#        require => Package['hphealth'],
+#        require => Class['snmp'],
+      }
 
-  exec { 'hpsnmpconfig':
-    command     => '/sbin/hpsnmpconfig',
-    environment => [
-      'CMASILENT=yes',
-      'CMALOCALHOSTRWCOMMSTR=8t0BAcw4Fjop9IrS4',
-     #'CMASYSCONTACT=',
-     #'CMASYSLOCATION=',
-     #'CMAMGMTSTATIONROCOMMSTR=',
-     #'CMAMGMTSTATIONROIPORDNS=',
-     #'CMATRAPDESTINATIONCOMMSTR=',
-     #'CMATRAPDESTINATIONIPORDNS='
-    ],
-#    subscribe => File['snmpd.conf'],
-#    creates => '/etc/hp-snmp-agents.conf',
-    refreshonly => true,
-    require     => Package['hp-snmp-agents'],
-    before      => Service['hp-snmp-agents'],
-    notify      => Service['snmpd'],
-  }
+      file { 'snmpd.conf-HP':
+        ensure  => $file_ensure,
+        mode    => '0660',
+        owner   => 'root',
+        group   => 'hpsmh',
+        path    => '/etc/snmp/snmpd.conf-HP',
+        source  => [
+          "puppet:///modules/snmp/snmpd.conf-${::fqdn}",
+          'puppet:///modules/snmp/snmpd.conf',
+        ],
+        require => Package['snmpd'],
+       #notify  => Service['snmpd'],
+        notify  => Exec['copy_snmpd.conf-HP'],
+      }
 
-  file { 'cma.conf':
-    ensure  => 'present',
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['hp-snmp-agents'],
-    path    => '/opt/hp/hp-snmp-agents/cma.conf',
-    source  => [
-      "puppet:///modules/psp/cma.conf-${::fqdn}",
-      'puppet:///modules/psp/cma.conf',
-    ],
-    notify  => Service['hp-snmp-agents'],
-  }
+      exec { 'copy_snmpd.conf-HP':
+        command     => '/bin/cp -p /etc/snmp/snmpd.conf-HP /etc/snmp/snmpd.conf',
+       #onlyif      => 'test -f /etc/snmp/snmpd.conf-HP',
+        require     => File['snmpd.conf-HP'],
+        notify      => Exec['hpsnmpconfig'],
+        refreshonly => true,
+      }
 
-  service { 'hp-snmp-agents':
-    ensure     => 'running',
-    name       => 'hp-snmp-agents',
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-    require    => Package['hp-snmp-agents'],
+      exec { 'hpsnmpconfig':
+        command     => '/sbin/hpsnmpconfig',
+        environment => [
+          'CMASILENT=yes',
+          'CMALOCALHOSTRWCOMMSTR=8t0BAcw4Fjop9IrS4',
+         #'CMASYSCONTACT=',
+         #'CMASYSLOCATION=',
+         #'CMAMGMTSTATIONROCOMMSTR=',
+         #'CMAMGMTSTATIONROIPORDNS=',
+         #'CMATRAPDESTINATIONCOMMSTR=',
+         #'CMATRAPDESTINATIONIPORDNS='
+        ],
+#        subscribe => File['snmpd.conf'],
+#        creates => '/etc/hp-snmp-agents.conf',
+        refreshonly => true,
+        require     => Package['hp-snmp-agents'],
+        before      => Service['hp-snmp-agents'],
+        notify      => Service['snmpd'],
+      }
+
+      file { 'cma.conf':
+        ensure  => $file_ensure,
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        path    => '/opt/hp/hp-snmp-agents/cma.conf',
+        source  => [
+          "puppet:///modules/psp/cma.conf-${::fqdn}",
+          'puppet:///modules/psp/cma.conf',
+        ],
+        require => Package['hp-snmp-agents'],
+        notify  => Service['hp-snmp-agents'],
+      }
+
+      service { 'hp-snmp-agents':
+        ensure     => $service_ensure_real,
+        enable     => $service_enable_real,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Package['hp-snmp-agents'],
+      }
+    }
+    # If we are not on HP hardware, do not do anything.
+    default: { }
   }
 }
