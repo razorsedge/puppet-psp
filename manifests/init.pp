3fb9e8b969095b1a351affe5effa33f1236d65da
Lukas Plattner <lukas.plattner@apa.at>
2014-07-30 11:05:43 +0200

    Make the yumconfig configurable via hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,26 @@
 #   Ensure if present or absent.
 #   Default: present
 #
+# [*yum_server*]
+#   Servername where the repo is
+#   Default: 'http://downloads.linux.hp.com'
+#
+# [*yum_os*]
+#   OS-Name for the Repopath
+#   Default: Depending on the OS
+#
+# [*yum_path*]
+#   Path on the Server to the repos
+#   Default: '/SDR/downloads/proliantsupportpack'
+#
+# [*yum_priority*]
+#   
+#   Default: '50'
+#
+# [*yum_project*]
+#   
+#   Default: '0'
+#
 # === Actions:
 #
 # Installs the SMH user and group as well as the YUM repository.
@@ -37,9 +57,14 @@
 # Copyright (C) 2012 Mike Arnold, unless otherwise noted.
 #
 class psp (
-  $ensure  = 'present',
-  $smh_gid = $psp::params::gid,
-  $smh_uid = $psp::params::uid
+  $ensure       = 'present',
+  $smh_gid      = $psp::params::gid,
+  $smh_uid      = $psp::params::uid,
+  $yum_server   = $psp::params::yum_server,
+  $yum_os       = $psp::params::yum_os,
+  $yum_path     = $psp::params::yum_path,
+  $yum_priority = $psp::params::yum_priority,
+  $yum_project  = $psp::params::yum_project,
 ) inherits psp::params {
 
   case $ensure {
@@ -73,10 +98,10 @@ class psp (
         descr    => 'HP Software Delivery Repository for Proliant Support Pack',
         enabled  => 1,
         gpgcheck => 1,
-        gpgkey   => "${psp::params::yum_server}${psp::params::yum_path}/GPG-KEY-ProLiantSupportPack",
-        baseurl  => "${psp::params::yum_server}${psp::params::yum_path}/${psp::params::yum_os}/\$releasever/packages/\$basearch/",
-        priority => $psp::params::yum_priority,
-        protect  => $psp::params::yum_protect,
+        gpgkey   => "${yum_server}${yum_path}/GPG-KEY-ProLiantSupportPack",
+        baseurl  => "${yum_server}${yum_path}/${yum_os}/\$releasever/packages/\$basearch/",
+        priority => $yum_priority,
+        protect  => $yum_protect,
       }
     }
     # If we are not on HP hardware, do not do anything.
